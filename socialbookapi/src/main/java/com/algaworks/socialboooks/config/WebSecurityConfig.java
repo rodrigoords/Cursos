81f15f57143a6c9ef4af0954ebac8237110d02ca
Rodrigo Sene <rodrigoords@hotmail.com>
2016-11-27 17:38:33 -0200

    Finalizado

--- socialbookapi/src/main/java/com/algaworks/socialboooks/config/WebSecurityConfig.java
+++ socialbookapi/src/main/java/com/algaworks/socialboooks/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
-package com.algaworks.socialboooks.congif;
+package com.algaworks.socialboooks.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
@@ -18,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.
 			authorizeRequests()
 			.antMatchers("/h2-console/**").permitAll()
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 				.httpBasic()
